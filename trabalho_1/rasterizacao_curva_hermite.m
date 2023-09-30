e701a664fa4488ac8ccc6a5537e31907776cc00e
Rudigus <rodrigo.matos9@hotmail.com>
2023-09-29 22:15:23 -0300

    mais curvas

--- trabalho_1/rasterizacao_curva_hermite.m
+++ trabalho_1/rasterizacao_curva_hermite.m
@@ -31,7 +31,9 @@ function img = rasterizarCurva(curva, numPontos, img)
     end
 end
 
-curvasNormalizadas = {[0 0; 0.4 0.4; 0 -1; 0 -1]};
+curvasNormalizadas = {[0.5 0.4; 0.9 0.8; 0.5 -1; 0.5 -1] [-0.5 0.6; 0.3 0.6; -0.5 1; 0.3 1] ...
+                      [-0.8 0; -0.4 0; 2.2 -2; 0 1.5] [0.7 -0.8; 0.7 -0.8; -2 -0.2; 2 -0.2] ...
+                      [0 0; 0.8 -0.4; -5.8 1.8; -3 -1]};
 % Defina as resoluções desejadas
 resolucoes = [100 100; 300 300; 800 600; 1920 1080];
 % Loop através das diferentes resoluções
