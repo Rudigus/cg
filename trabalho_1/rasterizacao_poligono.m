3678b82f077ec8eee8e34c34710b8df452b9a3d3
Rudigus <rodrigo.matos9@hotmail.com>
2023-10-31 18:03:02 -0300

    finaliza a rasterizacao de poligonos

--- trabalho_1/rasterizacao_poligono.m
+++ trabalho_1/rasterizacao_poligono.m
@@ -2,37 +2,40 @@
 1;
 
 function poligonoTela = poligonoNormParaTela(poligonoNormalizado, resolucao)
-    poligonoTela = zeros(4,2);
-    for i = 1:4
+    numPontos = rows(poligonoNormalizado);
+    poligonoTela = zeros(numPontos,2);
+    for i = 1:numPontos
         poligonoTela(i,:) = pontoNormParaTela(poligonoNormalizado(i,:), resolucao);
     end
 end
 
 function img = rasterizarPoligono(poligono, img)
-    % Rasterizar as arestas do polígono
+    numLinhasImg = rows(img);
+    numColunasImg = columns(img);
+    % É utilizado um canvas auxiliar para a pintura correta do polígono
+    imgAux = zeros(numLinhasImg, numColunasImg);
+    % Rasteriza as arestas do polígono
     numVertices = length(poligono);
     for i = 1:numVertices - 1
         reta = [poligono(i,:); poligono(i + 1,:)];
-        img = rasterizarReta(reta, img);
+        imgAux = rasterizarReta(reta, imgAux);
     end
     retaFinal = [poligono(numVertices,:); poligono(1, :)];
-    img = rasterizarReta(retaFinal, img);
-    % Pintar o interior do polígono por meio de scanline
-    numLinhas = rows(img);
-    numColunas = columns(img);
-    for i = 1:numLinhas
+    imgAux = rasterizarReta(retaFinal, imgAux);
+    % Pinta o interior do polígono por meio de scanline
+    for i = 1:numLinhasImg
         numIntersecoes = 0;
-        for j = 1:numColunas-1
+        for j = 1:numColunasImg-1
             % A interseção é contada logo antes de entrar no interior do polígono
-            if img(i, j) == 1 && img(i, j + 1) != 1
+            if imgAux(i, j) == 1 && imgAux(i, j + 1) != 1
                 if mod(numIntersecoes, 2) == 1
                     numIntersecoes += 1;
                     continue;
                 else
                     % Checa se existe aresta no restante das colunas para
                     % evitar a contagem de um vértice indevido
-                    for k = j+2:numColunas
-                        if img(i, k) == 1
+                    for k = j+2:numColunasImg
+                        if imgAux(i, k) == 1
                             numIntersecoes += 1;
                             break;
                         end
@@ -41,14 +44,20 @@ function img = rasterizarPoligono(poligono, img)
             end
             % Efetivamente pinta o interior do polígono
             if mod(numIntersecoes, 2) == 1
-                img(i, j) = 1;
+                imgAux(i, j) = 1;
             end
         end
     end
+    % Insere o polígono no canvas principal
+    img = bitor(img, imgAux);
 end
 
-poligonosNormalizados = {[0.0 0.0; 0.2 0.2; 0.4 0.0; 0.2 -0.2]};
-%{[0.0 0.0; 0.2 0.2; 0.4 0.0; 0.2 -0.2], [0.6 0.0; 0.6 0.2; 0.8 0.2; 0.8 0.0]};
+poligonosNormalizados = ...
+{[0.3 0.9; 0.6 (0.9 - 0.3 * sqrt(3)); 0.9 0.9] [-0.9 -0.9; -0.9 -0.6; (-0.9 + 0.15 * sqrt(3)) -0.75] ...
+[0.6 0.0; 0.6 0.2; 0.8 0.2; 0.8 0.0] [-0.7 0.9; -0.7 0.6; -0.4 0.6; -0.4 0.9] ...
+[-0.2 -0.1; 0.2 -0.1; 0.4 (-0.1 + 0.2 * sqrt(3)); 0.2 (-0.1 + 0.4 * sqrt(3));
+-0.2 (-0.1 + 0.4 * sqrt(3)); -0.4 (-0.1 + 0.2 * sqrt(3))] ...
+[0.5 -0.1; 0.6 -0.1; 0.8 -0.65; 0.75 -0.85; 0.5 -0.9; 0.4 -0.7]};
 % Defina as resoluções desejadas
 resolucoes = [100 100; 300 300; 800 600; 1920 1080];
 % Loop através das diferentes resoluções
